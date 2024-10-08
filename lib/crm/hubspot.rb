ad1a403a458e701d689d7351d85963c51f576b3c
Thuy Anh Nguyen <nthuyanh.hut@gmail.com>
2024-10-08 15:59:49 +0200

    Get deal from contact_id

--- lib/crm/hubspot.rb
+++ lib/crm/hubspot.rb
@@ -2,20 +2,25 @@
 
 module Crm
   class Hubspot
-    attr_reader :client
+    attr_reader :client, :user_id
 
-    def initialize(user_id:)
+    def initialize(user_id: nil)
       @client = ::Hubspot::Client.new(access_token: ENV["HUBSPOT_API_KEY"])
-      @contact_id = user_id
+      @user_id = user_id || 32_038_733_775
     end
 
-    def get_contact(id:)
-      client.crm.contacts.basic_api.get_by_id(contact_id: id)
+    def contact
+      client.crm.contacts.basic_api.get_by_id(contact_id: user_id, associations: :deal)
+    end
+
+    def deal
+      client.crm.deals.basic_api.get_by_id(
+        deal_id: contact.associations["deals"].results.first.id,
+        properties: [:documentfiletest]
+      )
     end
 
     def document
-      deal = client.crm.deals.basic_api.get_by_id(deal_id: 16_462_742_719, properties: [:documentfiletest])
-
       client.files.files_api.get_signed_url(file_id: deal.properties["documentfiletest"])
     end
   end
