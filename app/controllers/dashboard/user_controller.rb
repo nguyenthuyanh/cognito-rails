14c5251ff9fc9b5b64910af8e39a0d1b0522f652
Thuy Anh Nguyen <nthuyanh.hut@gmail.com>
2024-10-11 18:11:10 +0200

    Get file by attribute name and add file mapping

--- app/controllers/dashboard/user_controller.rb
+++ app/controllers/dashboard/user_controller.rb
@@ -1,9 +1,11 @@
 module Dashboard
   class UserController < ApplicationController
     def index
-      user = Crm::Hubspot.new
+      crm = Crm::Hubspot.new(user_id: 32_038_733_775)
+      contact = crm.get_contact(contact_id: 32_038_733_775, associations: :deal)
+      documents = crm.get_deal_files(id: contact.associations["deals"].results.first.id)
 
-      @document = user.document
+      render locals: { documents: }
     end
   end
 end
--- lib/crm/hubspot.rb
+++ lib/crm/hubspot.rb
@@ -4,24 +4,50 @@ module Crm
   class Hubspot
     attr_reader :client, :user_id
 
+    OBJECTS = [:contact, :deal].freeze
+
+    CONTACT_FILES = []
+
+    FILES_MAP = {
+      contact: {
+        first_document: :document_2,
+        second_document: :contact_document,
+      },
+      deal: {
+        document_test: :documentfiletest,
+        mairie_document: :mairie_document,
+        contract_document: :contract_document,
+      },
+    }
+
     def initialize(user_id: nil)
       @client = ::Hubspot::Client.new(access_token: ENV["HUBSPOT_API_KEY"])
-      @user_id = user_id || 32_038_733_775
+      @user_id = user_id
     end
 
-    def contact
-      client.crm.contacts.basic_api.get_by_id(contact_id: user_id, associations: :deal)
+    OBJECTS.each do |object_name|
+      define_method("get_#{object_name}") do |args|
+        client.crm.send(object_name.to_s.pluralize).basic_api.get_by_id(args)
+      end
+
+      define_method("get_#{object_name}_files") do |id:, file_attrs: nil|
+        file_attrs ||= FILES_MAP[object_name.to_sym].values
+        files = send("get_#{object_name}", "#{object_name}_id": id, properties: file_attrs).properties.symbolize_keys
+
+        file_attrs.each_with_object({}) do |attr, hash|
+          next if files[attr].blank?
+
+          file_ids = files[attr].split(";")
+          file_urls = file_ids.map { |file_id| get_file_url(file_id) }
+
+          hash[attr] = file_urls
+        end
+      end
     end
 
-    def deal
-      client.crm.deals.basic_api.get_by_id(
-        deal_id: contact.associations["deals"].results.first.id,
-        properties: [:documentfiletest]
-      )
-    end
-
-    def document
-      client.files.files_api.get_signed_url(file_id: deal.properties["documentfiletest"])
-    end
+    private
+      def get_file_url(id)
+        client.files.files_api.get_signed_url(file_id: id)
+      end
   end
 end
