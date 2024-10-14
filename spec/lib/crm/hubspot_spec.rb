4608d5f9641d962149c348686d9130690f96cb24
Thuy Anh Nguyen <nthuyanh.hut@gmail.com>
2024-10-14 18:58:40 +0200

    clean

--- spec/lib/crm/hubspot_spec.rb
+++ spec/lib/crm/hubspot_spec.rb
@@ -3,8 +3,6 @@ require "rails_helper"
 RSpec.describe ::Crm::Hubspot, type: :lib do
   subject(:crm) { described_class.new }
 
-  let(:attr_mapping) { { foo: { files: { attr1: "propertie_1", attr2: "propertie_2" } } } }
-
   described_class::ATTR_MAPPING.each_key do |object|
     describe "#get_#{object}" do
       it { is_expected.to respond_to("get_#{object}") }
