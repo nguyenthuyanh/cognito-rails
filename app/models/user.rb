c23fbb76dbd4400ea7134cb4aa2ccfe24a3ad189
Thuy Anh <nthuyanh.hut@gmail.com>
2024-10-29 10:01:41 +0100

    Feat/connect pennylane (#6)
    * Add quote download url
    
    * Add pennylane lib and add user model

---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ApplicationRecord
+  attr_accessor :first_name, :last_name, :email, :phone
+end
---
+++ lib/crm/pennylane.rb
@@ -0,0 +1,69 @@
+module Crm
+  class ApiResponseError < StandardError; end
+  class PennylaneError < StandardError; end
+
+  class Pennylane
+    class << self
+      attr_accessor :api_token, :host, :api_path
+
+      def setup
+        yield self
+      end
+
+      def headers
+        {
+          Authorization: "Bearer #{api_token}",
+          accept: "application/json",
+        }
+      end
+
+      def api_request(method: :post, endpoint: nil, queries: {}, body: nil)
+        url = get_url(endpoint, queries)
+
+        begin
+          response = HTTParty.send(method, url, headers:, body:)
+        rescue StandardError => e
+          response = e.response
+
+          message = "Method: #{method}, Url: #{url}, Payload: #{payload}, Response: #{response}"
+          Rails.logger.debug ApiResponseError.new(message)
+        end
+
+        response.deep_symbolize_keys
+      end
+
+      private
+        def get_url(endpoint, queries)
+          URI::HTTPS.build(
+            host:,
+            path: "/#{api_path}/#{endpoint}",
+            query: queries.to_query
+          ).to_s
+        end
+    end
+
+    def get_client_by_reference(reference)
+      filters = [{ field: "reference", operator: "eq", value: reference }].to_json
+      response = self.class.api_request(
+        method: :get,
+        endpoint: "customers",
+        queries: { filter: filters }
+      )
+
+      if response[:total_customers] == 1
+        return response[:customers].first
+      else
+        raise PennylaneError.new("Found #{response[:total_customers]} customers with same reference")
+      end
+    end
+
+    def get_invoices_by_client_id(id)
+      filters = [{ field: "customer_id", operator: "eq", value: id }].to_json
+      self.class.api_request(
+        method: :get,
+        endpoint: "customer_invoices",
+        queries: { filter: filters }
+      )[:invoices]
+    end
+  end
+end
